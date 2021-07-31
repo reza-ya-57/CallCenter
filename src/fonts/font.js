11545b354ed3c89dd0fab21acab3341cf63a91a1
reza_yari <rezaayaari.57@gmail.com>
2021-07-31 20:58:54 +0430

    sample

--- src/fonts/font.js
+++ src/fonts/font.js
@@ -1,18 +1,4 @@
-import IranSansttf from "./assets/Iranian_Sans.ttf";
-console.log(IranSansttf)
-export const IranSans = {
-  fontFamily: 'IranSans',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 400,
-  src: `
-    local('IranSans'),
-    local('IranSans-Regular'),
-    url(${IranSansttf}) format('ttf')
-  `,
-  unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-};
+
 
 
 // import RalewayWoff2 from './fonts/Raleway-Regular.woff2';
